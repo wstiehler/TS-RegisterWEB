5dcdd7ec569a856841e4fb598180f85666e40ad7
wstiehler <william.stiehler@evoluinfo.com.br>
2021-05-25 13:57:42 -0300

    Upload project django

--- config/settings.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-class Settings:
-
-    Engine = create_engine('postgresql+psycopg2://olist:olist123@localhost:5460/postgres_ts', echo=False)
-    Base = declarative_base()
